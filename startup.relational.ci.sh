79ed476057adbf029efbcced84e35315f49a9e7f
Nam Dang <namquoc.dev@gmail.com>
2025-06-09 21:42:18 +0700

    chore(ci): tmp disable e2e testing

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -6,6 +6,6 @@ npm run migration:run
 npm run seed:run:relational
 npm run start:prod > prod.log 2>&1 &
 /opt/wait-for-it.sh ${MAILDEV_HOST:-maildev}:${MAILDEV_PORT:-1080}
-/opt/wait-for-it.sh ${API_HOST:-localhost}:${API_PORT:-3000}
+#/opt/wait-for-it.sh ${API_HOST:-localhost}:${API_PORT:-3000}
 npm run lint
-npm run test:e2e -- --runInBand
+#npm run test:e2e -- --runInBand
